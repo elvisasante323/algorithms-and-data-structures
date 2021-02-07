43282d936be813600c761da1f89bb403532d7048
elvisasante5@yahoo.com <elvisasante5@yahoo.com>
2021-02-07 17:47:28 +0000

    Added and learnt a new data structure

---
+++ maps.py
@@ -0,0 +1,10 @@
+"""
+  Python has a powerful implementation of maps called dict. Dict helps you associate information with a hashable.
+"""
+
+dictionary = {
+    'alimony': 'Money given to woman after divorce',
+    'divorce': 'Legal separation of two legally married couples'
+}
+
+print(dictionary.values())
